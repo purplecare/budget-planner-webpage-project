a7a8c9601a73078099cdfcb832f5cdfaf6548f50
Simon2233 <simon.fu.1997@gmail.com>
2020-02-22 15:45:44 -0500

    dynamic row delete button fix

--- js/functions-budget.js
+++ js/functions-budget.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 		var category = $(".dropdown-category").val();
 		var itemName = $(".item-name").val();
 		var itemAmount= $(".item-amount").val();
-		$("div.monthly-payments table tbody").append("<tr><td>" + category + "</td><td>" + itemName + "</td><td>" + itemAmount + "</td><td><button type='button' name='button'>Edit</button></td><td><button type='button' name='button'>Delete</button></td></tr>");
+		$("div.monthly-payments table tbody").append("<tr><td>" + category + "</td><td>" + itemName + "</td><td>" + itemAmount + "</td><td><button type='button' name='button'>Edit</button></td><td><button class='delete-row' type='button' name='button'>Delete</button></td></tr>");
 	});
 	$(".delete-row").click(function() {
 		$(this).closest("tr").remove();
